21978f9a045b863ce36886c268fa8e5ba4dca6c8
Kyle <huangkyle110@gmail.com>
2021-03-04 14:20:54 -0800

    Added fourth test case, if year is not div by 4

--- leapyear_test.py
+++ leapyear_test.py
@@ -23,5 +23,24 @@ class LeapYearTest(unittest.TestCase):
         sys.stdout = out
         leap_year(100)
         sys.stdout = sys.__stdout__
-        self.assertEqual(out.getvalue(), "100 is a leap year\n")
-        
+        self.assertEqual(out.getvalue(), "100 is not a leap year\n")
+
+    # Good Case
+    # Test if year is div by 4 and div by 100 and div by 400
+    def test_div_400(self):
+        # Redirect stdout and capture output
+        out = io.StringIO()
+        sys.stdout = out
+        leap_year(2000)
+        sys.stdout = sys.__stdout__
+        self.assertEqual(out.getvalue(), "2000 is a leap year\n")
+
+    # Bad Case
+    # Test if year is not div by 4
+    def test_not_div_4(self):
+        # Redirect stdout and capture output
+        out = io.StringIO()
+        sys.stdout = out
+        leap_year(1)
+        sys.stdout = sys.__stdout__
+        self.assertEqual(out.getvalue(), "1 is not a leap year\n")
