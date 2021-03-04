400bcbdbffea3630206c910f5bf71174c2c19e31
Kyle <huangkyle110@gmail.com>
2021-03-04 14:12:14 -0800

    Wrote second test case, if year is div by 100 and 4 then its not a leap year

--- leapyear_test.py
+++ leapyear_test.py
@@ -5,6 +5,7 @@ from leapyear import leap_year
 
 
 class LeapYearTest(unittest.TestCase):
+    # Good Case
     # Test if year is divisible by 4
     def test_div_by_4(self):
         # Redirect stdout and capture output
@@ -13,3 +14,14 @@ class LeapYearTest(unittest.TestCase):
         leap_year(2016)
         sys.stdout = sys.__stdout__
         self.assertEqual(out.getvalue(), "2016 is a leap year\n")
+
+    # Bad Case
+    # Test if year is divisible by 100 and 4
+    def test_div_100(self):
+        # Redirect stdout and capture output
+        out = io.StringIO()
+        sys.stdout = out
+        leap_year(100)
+        sys.stdout = sys.__stdout__
+        self.assertEqual(out.getvalue(), "100 is a leap year\n")
+        
