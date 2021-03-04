bba8c49163373723a49f98bdda0269ff0f57eb3f
Kyle <huangkyle110@gmail.com>
2021-03-04 14:15:20 -0800

    Implemented leap year method - checks if year is div by 4 and exclude if div by 100

--- leapyear.py
+++ leapyear.py
@@ -1,4 +1,7 @@
 # Method for checking if given year is leap year
 def leap_year(n):
     if n % 4 == 0:
-        print(n, "is a leap year")
+        if n % 100 == 0:
+            print(n, "is not a leap year")
+        else:
+            print(n, "is a leap year")
