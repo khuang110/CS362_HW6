dd0c5faabd5a8bb574fb3656962f22038825aa97
Kyle <huangkyle110@gmail.com>
2021-03-04 14:18:47 -0800

    Implemented leap_year method - if year is div by 4 and 400 its a leap year

--- leapyear.py
+++ leapyear.py
@@ -1,7 +1,9 @@
 # Method for checking if given year is leap year
 def leap_year(n):
     if n % 4 == 0:
-        if n % 100 == 0:
+        if n % 400 == 0:
+            print(n, "is a leap year")
+        elif n % 100 == 0:
             print(n, "is not a leap year")
         else:
             print(n, "is a leap year")
