87a80abe682ea933af448ebc664f8f761167e13c
Kyle <huangkyle110@gmail.com>
2021-03-04 13:06:03 -0800

    Wrote first test, number not 1-100

--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -3,6 +3,6 @@ from fizzbuzz import fizz_buzz
 
 
 class TestCase(unittest.TestCase):
-    # test case if it is not number 1-100
+    # test case if it is not number 1-100.
     def test_fizz1(self):
         self.assertIsNone(fizz_buzz(0))
