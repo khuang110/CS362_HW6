594df5f5455d23f83c7d6ea25982aaee85dc5754
Kyle <huangkyle110@gmail.com>
2021-03-04 13:23:25 -0800

    Wrote second test case, Good case (1-100) not mult of 3 or 3 and 5

--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -1,4 +1,6 @@
 import unittest
+import io
+import sys
 from fizzbuzz import fizz_buzz
 
 
@@ -6,3 +8,13 @@ class TestCase(unittest.TestCase):
     # test case if it is not number 1-100.
     def test_fizz1(self):
         self.assertIsNone(fizz_buzz(0))
+
+    # Test case for a good number between 1-100.
+    # Not a multiple of 3 or 3 and 5
+    def test_fizz2(self):
+        # Redirect stdout and capture output
+        out = io.StringIO()
+        sys.stdout = out
+        fizz_buzz(1)
+        sys.stdout = sys.__stdout__
+        self.assertEqual(out.getvalue(), "1\n")
